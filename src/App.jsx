ed60d68cb82834f8ca2ab160cbe113a1d4460ff9
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-01-18 19:22:14 +0530

    Redux store integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth.js"
+import { login, logout } from './store/authSlice.js';
+import { Footer, Header } from './components/index.js';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login(userData)); // This make changes in the store and tell the store that i user is logged in so that we can ask the store the if a user is logged in or not anywhere
+      } else {
+        dispatch(logout()); // This tells the user has been logged out. If it is not written then the store will not change its status even after the user has logged out
+      }
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+  }, []);
+
+  if(loading) {
+    return <div>Loading...</div>
+  }
 
   return (
-    <>
-      <h1>Bhalu Biran Ka Blog App</h1>
-    </>
+    <div className='min-h-screen flex justify-center items-center flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
   )
 }
 
